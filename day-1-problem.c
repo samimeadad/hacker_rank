4bea9efbbca2c010e47ad17fac6c73c6ad0ff088
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-08-02 19:56:15 +0600

    problem 1 partial

---
+++ day-1-problem.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+    // Declare second integer, double, and String variables.
+    int secondInt = 0;
+    double doubleVar = 0;
+    char stringVar[100];
+
+    int intSum = 0;
+    double doubleSum = 0;
+
+    // Read and save an integer, double, and String to your variables.
+    scanf("%d", &secondInt);
+    scanf("%lf", &doubleVar);
+    scanf("%[^\n]%*c", &stringVar);
+    //fgets(stringVar, 100, stdin);
+
+    intSum = i + secondInt;
+    doubleSum = d + doubleVar;
+    strcat(s, stringVar);
+
+    // Print the sum of both integer variables on a new line.
+    printf("\n");
+    printf("%d", intSum);
+
+    // Print the sum of the double variables on a new line.
+    printf("\n");
+    printf("%0.1lf", doubleSum);
+
+    // Concatenate and print the String variables on a new line
+    printf("\n");
+    puts(s);
+    // The 's' variable above should be printed first.
+
+    return 0;
+}
